9448c0f5f43d35e79a2c8deb5f091c3c0d16be55
Mateusz Migas <mateuszmigas.dev@gmail.com>
2020-08-14 22:30:28 +0200

    CSS styles

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -1,10 +1,38 @@
 import React from "react";
-import "./App.css";
 import { SimpleTextDropdown } from "@mateuszmigas/dropdown";
+import "./styles.css";
+import { randomNames } from "./names";
+
+export function MyDropdown() {
+  const [isOpen, setIsOpen] = React.useState(true);
+  return (
+    <div className="dropdown-container">
+      <div className="dropdown-main">
+        <button
+          onClick={() => setIsOpen((o) => !o)}
+          className="dropdown-select"
+          value="f"
+        ></button>
+        <button className="dropdown-close">bt</button>
+      </div>
+      {isOpen && (
+        <div className="dropdown-list">
+          <div>
+            {randomNames.map((name) => {
+              return <div className="dropdown-list-item">{name}</div>;
+            })}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
+      <MyDropdown></MyDropdown>
+
       <SimpleTextDropdown></SimpleTextDropdown>
     </div>
   );
